97ac63d971fbfda614aedcce7b09c04663feea52
PalakVanjara <palakvanjara38@gmail.com>
2026-02-05 13:42:29 +0530

    create object file

---
+++ object.js
@@ -0,0 +1,31 @@
+// Objects vs Array
+// Objects -- when you want to show all data based on
+// one entity(k j entity mate all data jota hoy) (ex.
+// one User data)
+
+// Array -- when you want to show entity (ex. all
+// Users)
+
+// create
+// varabile = {}
+// key : value --> key-value sturcture
+let User ={
+    name:"Test",
+    email:"test@gmail.com",
+    rollno:7
+}
+// access (dot vs square notation)
+// variable_name.key;
+// User.first-name;
+User.name;
+User.email;
+
+// variable_name['key'];
+// User[first_name];
+User['name'];
+
+// let demo = "name"
+
+// User[demo] --> convert into name (demo = 'name')
+
+// --> output: abc
